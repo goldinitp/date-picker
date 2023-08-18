8871f3b1528e804422e24e33b5e243451df24610
Goldi Kumar <gkumar2@atlassian.com>
2023-08-18 10:35:16 +0530

    fix

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -19,10 +19,9 @@ import { DatePicker } from '@atlaskit/datetime-picker';
 import GlobalStyle from '../../global-styles';
 
 const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
+  width: 1280px;
   margin: 0 auto;
   display: flex;
-  min-height: 100%;
   padding: 0 16px;
   flex-direction: column;
 `;
